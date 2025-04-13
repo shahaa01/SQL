05c921fd742228311184e31e65fcfc51dcee2cd9
Aaditya Shah <aadishah1020@gmail.com>
2025-04-13 18:13:48 -0400

    Learned how to connect SQL with node

--- index.js
+++ index.js
@@ -11,4 +11,32 @@ let getRandomUser =  () => {
     };
   }
 
-  console.log(getRandomUser());
+
+  //Lets learn to connect node with mySQL
+  //we will use MySQL2 Package which helps to connect node with MySQL
+  //lets require it now 
+  const mysql = require('mysql2');
+
+  //it connects with the database that we created with SQL workbench
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "sarathi1",
+    password: "Sangita@#1020"
+  });
+
+    let query = "SHOW TABLES";
+    connection.query(query, (err, results) => {
+      if(err) {
+        return `The error is: ${err.message}`;
+      }
+      console.log(results); //this is an array which stores objects
+      console.log(results.length)
+    });
+
+    //the connection doesn't ends by itself - so to end the connection w the database
+    connection.end();
+
+console.log("Things are still working");
+
+//Now lets learn to use SQL from CLI instead of using workbench - refer to notes
